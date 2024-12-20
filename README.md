6ac3b16e6659463fdfbbfbcbb15ba48ce332d451
bhavarma <141211992+bhavarma@users.noreply.github.com>
2024-12-20 23:25:56 +0530

    Creating Rest API and also shown the use of middlware

--- README.md
+++ README.md
@@ -9,9 +9,18 @@
     - Must visit guide page for more info https://expressjs.com/en/guide/routing.html
     - Different methods to end request response cycle - https://expressjs.com/en/guide/routing.html#:~:text=)%0A%7D)-,Response%20methods,-The%20methods%20on 
 
+3. Create The Rest Api and understand Middlware
+    - understand use of the middlware ex. Here created the requestTime middlware which get called for all route who set the requiestTime property in req ogject which used in next function.
+    - Used https://www.mockaroo.com/ To generate json data.
+    - List of APIs 
+        - /api/users/     GET Method  (Get all users data)
+        - /api/users/     POST Method (Create user)
+        - /api/users/id   GET Method  (Get the user with id)
+        - /api/users/id   PATCH Method  (Update the user)
+        - /api/users/id   DELETE Method (Delete the user)
 
-Guide Pages : 
-    - Event loop - https://nodejs.org/en/learn/asynchronous-work/event-loop-timers-and-nexttick 
-    - Express APIs guide : https://expressjs.com/en/4x/api.html#express -> what happen when do require('express') https://stackoverflow.com/questions/42631107/when-express-is-required-is-it-a-function-or-an-object 
+
+Guide Pages :
+    - Event loop - https://nodejs.org/en/learn/asynchronous-work/event-loop-timers-and-nexttick
+    - Express APIs guide : https://expressjs.com/en/4x/api.html#express -> what happen when do require('express') https://stackoverflow.com/questions/42631107/when-express-is-required-is-it-a-function-or-an-object
     - Express guide https://expressjs.com/en/guide/routing.html - looks for other topics as well.
-
---
+++ Rest_API/index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const fs = require('fs');
+const users = require('./MOCK_DATA.json');
+
+const app = express();
+
+
+// Let's write requestTime middlware which add requestTime property in req object which further used in next function chain
+const requestTime = function (req, res, next) {
+    req.requestTime = Date.now();
+    next();
+};
+app.use(requestTime);
+
+app.use((req, res, next) => {
+    const log = `${req.requestTime} : ${req.url}\n`;
+    fs.appendFile('log.txt', log, (err) => {
+        if (err)
+            console.error(`Error writing log: ${err}`);
+        else
+            console.log(`Log added - ${log}`);
+    });
+    next();
+});
+
+app.get('/users', (req, res) => {
+    const html = `
+    <ul> 
+        ${users.map(user => `<li> ${user.first_name} </li>`).join(" ")}
+    </ul>
+    `
+    res.send(html);
+});
+
+app.get('/api/users', (req, res) => {
+    res.send(users);
+});
+
+
+function saveUsersToFile(users, res, successMessage) {
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
+        if (err) {
+            return res.status(500).send('Error saving data to file');
+        }
+        // Send the response after the file has been successfully updated
+        res.send(successMessage);
+    });
+}
+
+app.use(express.urlencoded({extended:false})); // This middlware set req.body
+app.post('/api/users', (req, res)=>{
+    const newUser = req.body;
+    // Check if required fields are present
+    if (!newUser.first_name || !newUser.last_name || !newUser.email) {
+        return res.status(400).send('Missing required fields');
+    }
+    users.push({id: users.length + 1, ...newUser});
+    saveUsersToFile(users,res,`New user added with ID: ${users.length}`);
+});
+
+app.route('/api/users/:id')
+    .all(function (req, res, next) {
+        // runs for all HTTP methods
+        // think of it as route specific middleware!
+        console.log('/api/users/:id get delete patch method hit');
+        next();
+    })
+    .get(function (req, res) {
+        const id = Number(req.params.id);
+        const user = users.find(user => user.id === id);
+        if (!user) {
+            // Return 404 response if user is not found
+            return res.status(404).send(`User with ID: ${id} not found.`);
+        }
+        res.send(user);
+    })
+    .delete(function (req, res) {
+        const id = Number(req.params.id);
+        // const updatedUsers = users.filter(user => user.id !== id);
+        const userIndex = users.findIndex(user => user.id === id);
+        users.splice(userIndex, 1);// Remove the user from the array
+        saveUsersToFile(users,res,`Deleted the user with ID : ${id}`);
+    })
+    .patch(function (req, res) {
+        const id = Number(req.params.id);
+        const user = users.find(user => user.id === id); 
+        if (!user) {
+            // Return 404 response if user is not found
+            return res.status(404).send(`User with ID: ${id} not found.`);
+        }
+        user.email = req.body.email || user.email;
+        saveUsersToFile(users,res,`User with ID: ${id} updated successfully.`);
+    });
+
+
+// Handle unknown routes (404)
+app.use((req, res) => {
+    res.status(404).send('404 - Page Not Found');
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
